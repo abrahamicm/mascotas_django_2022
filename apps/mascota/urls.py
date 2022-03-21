24d63dcc79a3f33841be38f34e0b6178403521a7
Abraham Cordero <abrahamicm2@gmail.com>
2022-03-21 14:55:59 -0400

    terminando video 13 listar mascotas

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from apps.mascota.views import index, mascota_view
+from apps.mascota.views import index, mascota_view,mascota_list
 
 urlpatterns = [
 
     path('', index, name='index'),
     path('nuevo',mascota_view , name='mascota_crear'),
+    path('listar', mascota_list, name='mascota_listar'),
  
 ]
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from apps.mascota.forms import MascotaForm
 
+from apps.mascota.forms import MascotaForm
+from apps.mascota.models import Mascota
 # Create your views here.
 def index(request):
     return render(request,"mascota/index.html")
@@ -15,3 +16,8 @@ def mascota_view(request):
 	else:
 		form = MascotaForm()
 	return render(request, 'mascota/mascota_form.html', {'form':form})
+
+def mascota_list(request):
+	mascota = Mascota.objects.all().order_by('id')
+	contexto = {'mascotas':mascota}
+	return render(request, 'mascota/mascota_list.html', contexto)
